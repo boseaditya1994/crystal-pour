6eb8b66784b1f81fd51205d58a1a62546fc3865f
boseaditya21 <AdityaBose@21>
2025-08-19 16:22:51 +0530

    Implemented the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </main>
+  );
 };
 
 export default App;
